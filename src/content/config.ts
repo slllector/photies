b69837f3c10dc6454023c9478389f910756cfa0a
Slllector <slllector0@gmail.com>
2025-04-21 17:14:39 +0100

    all galleries are active

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,27 +3,15 @@ import { cldAssetsLoader } from 'astro-cloudinary/loaders';
 
 export const collections = {
   urbanPhotos: defineCollection({
-    loader: cldAssetsLoader({
-      folder: 'web/urban',
-      context: true
-    })
+    loader: cldAssetsLoader({ folder: 'web/urban', context: true, limit: 500 })
   }),
   naturePhotos: defineCollection({
-    loader: cldAssetsLoader({
-      folder: 'web/nature',
-      context: true
-    })
+    loader: cldAssetsLoader({ folder: 'web/nature', context: true, limit: 500 })
   }),
   landscapePhotos: defineCollection({
-    loader: cldAssetsLoader({
-      folder: 'web/landscapes',
-      context: true
-    })
+    loader: cldAssetsLoader({ folder: 'web/landscapes', context: true, limit: 500 })
   }),
   plantPhotos: defineCollection({
-    loader: cldAssetsLoader({
-      folder: 'web/plants',
-      context: true
-    })
+    loader: cldAssetsLoader({ folder: 'web/plants', context: true, limit: 500 })
   }),
-}
+};
