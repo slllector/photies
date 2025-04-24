29618b381acb43973a61dff864ae26bf14b04504
Slllector <slllector0@gmail.com>
2025-04-24 22:33:29 +0100

    update entire site structure

--- src/utils/helpers.ts
+++
@@ -1,4 +0,0 @@
-export function getAltTextFromPublicId(publicId: string): string {
-  const fileName = publicId.split('/').pop() ?? '';
-  return fileName.substring(0, fileName.lastIndexOf('_'));
-}
